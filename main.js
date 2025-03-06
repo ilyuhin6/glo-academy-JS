87d680af7e7df29c683026f0a0eb554e626e02f8
ilyuhin6 <142712102+ilyuhin6@users.noreply.github.com>
2025-03-06 09:34:24 +0700

    Выполнил ДЗ согласно ТЗ)

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let title = 'Изучение JS' ;
-let screens = 'Простые, Сложные, Интерактивные' ;
-let screenPrice = 500 ;
+let title = 'Изучение JS';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 500;
 let rollback  = 10;
-let fullPrice = 5000 ;
-let adaptive = true ;
+let fullPrice = 5000;
+let adaptive = true;
+
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей ' + 'и ' + 'Стоимость разработки сайта ' + fullPrice + ' рублей');
+
+console.log(screens.toLocaleLowerCase().split(','));
+
+console.log('Процент отката посреднику за работу ' + fullPrice * (rollback / 100));
